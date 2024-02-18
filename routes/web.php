5fc4d61598ba6888c21e4c938a950ae081f9ce94
yourahmeddev <ahmedhassansheikh7@gmnail.com>
2024-02-18 18:40:46 +0500

    Routing done in laravel course

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// here we are creating our first route in laravel
+Route::get('/greeting', function(){
+return 'Hello world';
+});
+// parameterized Routes
+Route::get('/user/{id}', function($id){
+return 'User Id'.$id;
+});
+// optional parameterized Route
+Route::get('student/{name?}', function($name=null){
+   return 'Student Name'.$name;
+});
+// adding contrainsts to routes in laravel
+Route::get('/teacher/{id}', function($id){
+return 'User Id is'.$id;
+})->where('id','[0-9]+');
+// Riderecting from one route to another route
+Route::redirect('/test', '/greeting');
+// how to open route list in laravel
+//php artisan route:list
+// if we want to get without vendor 
+// php artisan route:list --except-vendor
+// How to redirect from one to another using href
+Route::get('/home', function(){
+return '<a href="/greeting">Greeting</a>';
+});
